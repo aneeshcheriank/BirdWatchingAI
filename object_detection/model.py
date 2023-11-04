08ed0cc8d2b906d438d5e7dc8312b4acf58d9657
aneeshcheriank <aneeshcheriank@gmail.com>
2023-11-04 09:07:55 +0100

    exposed threshold as an argument to the function in detect_object method

--- object_detection/model.py
+++ object_detection/model.py
@@ -14,6 +14,6 @@ def detect_objects(tensor, threshold=THRESHOLD):
         detections_batch = ssd_model(tensor)
 
     results_per_input = utils.decode_results(detections_batch)
-    best_results_per_input = [utils.pick_best(results, 0.40) for results in results_per_input]
+    best_results_per_input = [utils.pick_best(results, threshold) for results in results_per_input]
 
     return best_results_per_input
